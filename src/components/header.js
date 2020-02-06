8bbc34b80695170dad29a9cd2c324c518eb5a410
gabesharpton <gabriel.sharpton@gmail.com>
2020-02-06 16:07:07 -0600

    fixed some accessibility issues, ie. links dont have names and color contrast of title

--- src/components/header.js
+++ src/components/header.js
@@ -13,30 +13,34 @@ const Head = styled.div`
 
 const Header = () => (
   <Head>
-    <h1 id="title" style={{ paddingTop: 20, color: "#edbc64" }}>
+    <h1 id="title" style={{ paddingTop: 20, color: "black" }}>
       Gabriel Sharpton || Full-Stack Developer
     </h1>
     <Image style={{ height: `350px`, width: `300px` }} />
-
+<div id='profiles'>
     <a id="linkedin-profile" 
     target="_blank"
     rel="noopener noreferrer" 
     href="https://www.linkedin.com/in/gabesharpton/">
       <FaLinkedin />
+      <h5 style={{color: 'black'}}>Linkedin Profile</h5>
     </a>
     <a id="github-profile"
     target="_blank"
     rel="noopener noreferrer" 
     href="https://github.com/gabesharpton">
-      {" "}
-      <FaGithub />{" "}
+      
+      <FaGithub />
+      <h5>Github Profile</h5>
     </a>
     <a id="email-address" 
     target="_blank"
     rel="noopener noreferrer" 
     href="mailto:Gabriel.sharpton@gmail.com">
       <MdEmail />
+      <h5 style={{color: 'black'}}>Send email</h5>
     </a>
+    </div>
   </Head>
 );
 
